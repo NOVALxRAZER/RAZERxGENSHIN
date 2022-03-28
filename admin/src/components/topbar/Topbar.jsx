f795b1b0a9c1392a8d87cf3b12047bfe5898be52
noval <giannovalnutech77@gmail.com>
2022-03-28 10:08:18 +0700

    Update28Mar2022

--- admin/src/components/topbar/Topbar.jsx
+++ admin/src/components/topbar/Topbar.jsx
@@ -2,41 +2,42 @@ import React from "react";
 import "./topbar.css";
 import { NotificationsNone, Language, Settings } from "@material-ui/icons";
 import { userLogout } from "../../redux/apiCalls";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function Topbar() {
-  const myStorage = window.localStorage;
-  const dispatch = useDispatch();
+    const myStorage = window.localStorage;
+    const dispatch = useDispatch();
+    const user = useSelector(state => state.user.currentUser);
 
-  const handleClick = () => {
-    userLogout(dispatch);
-    myStorage.removeItem("persist:root");
-  }
+    const handleClick = () => {
+        userLogout(dispatch);
+        myStorage.removeItem("persist:root");
+    }
 
-  return (
-    <div className="topbar">
-      <div className="topbarWrapper">
-        <div className="topLeft">
-          <span className="logo">Razer Admin</span>
+    return (
+        <div className="topbar">
+            <div className="topbarWrapper">
+                <div className="topLeft">
+                    <span className="logo">Razer Admin</span>
+                </div>
+                <div className="topRight">
+                    <div className="topbarIconContainer">
+                        <a href="/login" style={{ textDecoration: "none", color: "black" }} onClick={handleClick}>Logout</a>
+                    </div>
+                    <div className="topbarIconContainer">
+                        <NotificationsNone />
+                        <span className="topIconBadge">2</span>
+                    </div>
+                    <div className="topbarIconContainer">
+                        <Language />
+                        <span className="topIconBadge">2</span>
+                    </div>
+                    <div className="topbarIconContainer">
+                        <Settings />
+                    </div>
+                    <img src={user.image} alt="" className="topAvatar" />
+                </div>
+            </div>
         </div>
-        <div className="topRight">
-          <div className="topbarIconContainer">
-            <a href="/login" style={{ textDecoration: "none", color: "black" }} onClick={handleClick}>Logout</a>
-          </div>
-          <div className="topbarIconContainer">
-            <NotificationsNone />
-            <span className="topIconBadge">2</span>
-          </div>
-          <div className="topbarIconContainer">
-            <Language />
-            <span className="topIconBadge">2</span>
-          </div>
-          <div className="topbarIconContainer">
-            <Settings />
-          </div>
-          <img src="https://upload.wikimedia.org/wikipedia/en/thumb/4/40/Razer_snake_logo.svg/1200px-Razer_snake_logo.svg.png" alt="" className="topAvatar" />
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
--- client/src/pages/ProductList.jsx
+++ client/src/pages/ProductList.jsx
@@ -19,18 +19,18 @@ const FilterContainer = styled.div`
 `
 const Filter = styled.div`
     margin: 20px;
-    ${mobile({width: "0px 20px", display: "flex", flexDirection: "column"})}
+    ${mobile({ width: "0px 20px", display: "flex", flexDirection: "column" })}
 `
 const FilterText = styled.div`
     font-size: 20px;
     font-weight: 600;
     margin-right: 20px;
-    ${mobile({marginRight: "0px"})}
+    ${mobile({ marginRight: "0px" })}
 `
 const Select = styled.select`
     padding: 10px;
     margin-right: 20px;
-    ${mobile({margin: "10px 0px"})}
+    ${mobile({ margin: "10px 0px" })}
 `
 
 const Option = styled.option``
@@ -51,7 +51,7 @@ export default function ProductList() {
 
     return (
         <Container>
-            <Navbar/>
+            <Navbar />
             <Title>{cat}</Title>
             <FilterContainer>
                 <Filter>
@@ -65,7 +65,7 @@ export default function ProductList() {
                         <Option value="white">White</Option>
                         <Option value="grey">Grey</Option>
                         <Option value="pink">Pink</Option>
-                        <Option value="red">Ref</Option>
+                        <Option value="red">Red</Option>
                         <Option value="blue">Blue</Option>
                         <Option value="yellow">Yellow</Option>
                     </Select>
@@ -79,9 +79,9 @@ export default function ProductList() {
                     </Select>
                 </Filter>
             </FilterContainer>
-            <Products cat={cat} filters={filters} sort={sort}/>
-            <Newsletter/>
-            <Footer/>
+            <Products cat={cat} filters={filters} sort={sort} />
+            <Newsletter />
+            <Footer />
         </Container>
     )
 }
